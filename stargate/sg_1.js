d2db6b37c8a6ac4b5ee03a4a5182a0110743ec80
Dariusz Hozer <33494247+dar23@users.noreply.github.com>
2023-05-12 09:32:22 +0200

    Add files via upload

---
+++ stargate/sg_1.js
@@ -0,0 +1,13 @@
+
+function show_stargate(){
+
+var show_stargate=document.querySelector('.stargate');
+var symbole=document.querySelector('.symbole');
+
+
+show_stargate.style.display='block';
+symbole.style.display='block';
+
+}
+
+setTimeout(show_stargate,2600);
